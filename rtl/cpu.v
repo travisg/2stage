0bf44683528bd86fcb865fc617a8242a5ce836b2
Travis Geiselbrecht <geist@foobox.com>
2014-10-12 01:46:22 -0700

    mostly working without multicycle stuff

--- rtl/cpu.v
+++ rtl/cpu.v
@@ -38,14 +38,13 @@ localparam IWIDTH = 16;
 /* first stage (instruction fetch) */
 reg [IADDRWIDTH-1:0] pc;
 logic [IADDRWIDTH-1:0] pc_next;
-reg [IADDRWIDTH-1:0] ir = 0;
-logic [IADDRWIDTH-1:0] ir_next;
+logic [IADDRWIDTH-1:0] s1_ifetch;
 
 assign iaddr = pc;
 
 always_comb begin
     pc_next = pc + 1; /* default to next instruction */
-    ir_next = idata; /* default to whatever is coming back on the instruction bus */
+    s1_ifetch = idata; /* default to whatever is coming back on the instruction bus */
 end
 
 always_ff @(posedge clk) begin
@@ -58,7 +57,6 @@ always_ff @(posedge clk) begin
         end else begin
             pc <= pc_next;
         end
-        ir <= ir_next;
     end
 end
 
@@ -67,52 +65,83 @@ always begin
 end
 
 /* second stage */
-logic [2:0] reg_d;
-logic [2:0] reg_a;
-logic [2:0] reg_b;
-logic [2:0] alu_op;
-logic decode_branch;
-logic [15:0] offset;
+reg [IADDRWIDTH-1:0] ir = 0;
+
+/* decoder */
+wire [3:0] op = ir[15:12];
+wire [2:0] reg_a = ir[11:9];
+wire reg_d_indirect = ir[8];;
+wire [2:0] reg_d = ir[7:5];
+wire [1:0] reg_b_addr_mode = ir[4:3];
+wire [2:0] reg_b = ir[2:0];
+
+logic [15:0] branch_offset;
 logic [15:0] s2_pc_next;
 logic s2_to_s1_take_branch;
 
 logic [15:0] reg_a_out;
 logic [15:0] reg_b_out;
-
 logic [15:0] alu_result;
+logic reg_writeback;
 
 always_comb begin
-    reg_a = ir[14:12];
-    reg_d = ir[11:9];
-    reg_b = ir[8:6];
-    decode_branch = ir[15];
     s2_pc_next = 16'bX;
     s2_to_s1_take_branch = 0;
-    alu_op = ir[2:0];
+    reg_writeback = 0;
     alu_result = 16'bX;
+    branch_offset = ir[8] ? { 7'b1111111, ir[8:0] } : { 7'b0, ir[8:0] };
 
-    // decode branch offset, sign extended to 16 bits
-    if (ir[11])
-        offset = { 5'b11111, ir[10:0] };
-    else
-        offset = { 5'b00000, ir[10:0] };
-
-    if (decode_branch) begin
-        s2_pc_next = pc + offset;
-        s2_to_s1_take_branch = (reg_a_out == 0);
-        $display("S2: ir %x, branch rd %d, offset %x, s2_pc_next %x, take branch %d", ir, reg_d, offset, s2_pc_next, s2_to_s1_take_branch);
-    end else begin
-        case (alu_op)
-        3'b000: begin // ADD
-            alu_result = reg_a_out + reg_b_out;
-        end
-///
-
-
-
-        endcase
-        $display("S2: ir %x, alu rd %d, ra %d, rb %d", ir, reg_d, reg_a, reg_b);
+    casez (op)
+    4'b110?: begin // branch
+        s2_pc_next = pc + branch_offset;
+        s2_to_s1_take_branch = ir[12] ? (reg_a_out != 0) : (reg_a_out == 0);
+        $display("S2: ir %x, branch rd %d, offset %x, s2_pc_next %x, take branch %d", ir, reg_d, branch_offset, s2_pc_next, s2_to_s1_take_branch);
     end
+    4'b111?: begin // undefined
+    end
+    default: begin // alu op
+        // handle the 2nd operand
+        logic [15:0] alu_b_in;
+        casez (reg_b_addr_mode)
+            2'b0?: begin // 4 bit signed immediate
+                alu_b_in = ir[3] ? { 12'b111111111111, ir[3:0] } : { 12'b0, ir[3:0] };
+            end
+            2'b10: begin
+                if (reg_b == 0)
+                    alu_b_in = 0; // XXX next instruction fetch handle
+                else
+                    alu_b_in = reg_b_out;
+            end
+            2'b11: begin // XXX register b indirect, handle
+                alu_b_in = 0;
+            end
+        endcase
+
+        // do the alu op
+        case (op)
+            4'b0000: alu_result = reg_a_out + alu_b_in;
+            4'b0001: alu_result = reg_a_out - alu_b_in;
+            4'b0010: alu_result = reg_a_out & alu_b_in;
+            4'b0011: alu_result = reg_a_out | alu_b_in;
+            4'b0100: alu_result = reg_a_out ^ alu_b_in;
+            4'b0101: alu_result = reg_a_out << alu_b_in;
+            4'b0110: alu_result = reg_a_out >> alu_b_in;
+            4'b0111: alu_result = reg_a_out >>> alu_b_in; // XXX check
+            4'b1000: alu_result = (reg_a_out == alu_b_in) ? 1 : 0;
+            4'b1001: alu_result = (reg_a_out < alu_b_in) ? 1 : 0;
+            4'b1010: alu_result = (reg_a_out <= alu_b_in) ? 1 : 0;
+            default: ;
+        endcase
+        reg_writeback = 1;
+        $display("S2: ir %x, wb %d, alu rd %d, ra %d, rb %d", ir, reg_writeback, reg_d, reg_a, reg_b);
+    end
+    endcase
+
+    // handle indirect d writebacks
+end
+
+always_ff @(posedge clk) begin
+    ir <= s1_ifetch;
 end
 
 /* register file */
@@ -126,9 +155,9 @@ regfile regs(
     .raddr_b(reg_b),
     .rdata_b(reg_b_out),
 
-    .we(0),
-    .waddr(0),
-    .wdata(0)
+    .we(reg_writeback),
+    .waddr(reg_d),
+    .wdata(alu_result)
 );
 
 endmodule
